0ef90f6eb927f3616ca85ae76d3655951f089971
itsMontoya <josh@montoya.io>
2017-11-13 13:02:37 -0800

    Add OnPop

--- router.ts
+++ router.ts
@@ -1,10 +1,14 @@
+import * as entry from "node_modules/pubsub/entry";
+
 const pushState = window.history.pushState;
 
 export class Router {
 	private routes: Route[];
+	private e: entry.Entry<string>;
 
 	constructor() {
 		this.routes = new Array();
+		this.e = new entry.Entry<string>("url")
 
 		window.history.pushState = (_: any, title: string, url: string) => this.handlePop(title, url);
 		window.onpopstate = () => this.handlePop("", window.location.pathname);
@@ -12,7 +16,11 @@ export class Router {
 
 	private handlePop(title: string, url: string) {
 		const parts = url.split("/");
-		this.routes.some((r: Route): boolean => r.Match(parts));
+		const match = this.routes.some((r: Route): boolean => r.Match(parts));
+		if (match) {
+			this.e.Put(url);
+		}
+		
 		pushState.apply(history, [null, title, url]);
 	}
 
@@ -24,6 +32,10 @@ export class Router {
 
 	Pop(title: string, url: string) {
 		window.history.pushState(null, title, url);
+	}
+
+	OnPop(fn: PopFn) {
+		this.e.Sub((_: string, url: string) => fn(url));
 	}
 }
 
@@ -74,3 +86,5 @@ export class Route {
 export type Handler = (p: Params) => void;
 
 export type Params = { [key: string]: string }
+
+export type PopFn = (url: string) => boolean;
